dda26b5bf594c452e5f71801544c1ef5df63b9e8
Opeyimika-sudo <54380080+Opeyimika-sudo@users.noreply.github.com>
2022-10-31 00:42:13 +0100

    init: add components

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+      <h1>Bro, watch me do great!</h1>
+    </div>
+  )
+}
---
+++ src/components/Todo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Todo() {
+  return (
+    <div>
+      <p>It will take some time but my greatness will be undisputable</p>
+    </div>
+  )
+}
