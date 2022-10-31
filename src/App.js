5b8ef63aa2bb8ce0f298ec255738cca45df846f2
Opeyimika-sudo <54380080+Opeyimika-sudo@users.noreply.github.com>
2022-10-31 03:38:15 +0100

    feat: create todo list string using useState and useEffect

--- src/App.js
+++ src/App.js
@@ -10,18 +10,25 @@ function App() {
       setToggleMode(prevState => !prevState)
   }
   
+  const [todoList, setTodoList] = React.useState(localStorage.getItem('todos'))
   const [formData, setFormData] = React.useState("")
 
   function handleChange(event){
-      setFormData(event.target.value)
-      console.log(event.target.value)
+    setFormData(event.target.value)
+    console.log(event.target.value)
   }
-    
-  function handleSubmit(event){
-        event.preventDefault();
-        console.log(formData)
-    }
 
+  function handleSubmit(event){
+    event.preventDefault();
+    localStorage.setItem('todos', todoList == null ? formData : [todoList, formData]);
+}
+
+  React.useEffect(() => {
+    setTodoList(localStorage.getItem('todos'));
+    console.log(todoList)
+  }, [localStorage.getItem('todos')])
+
+  
   return (
     <div className="App">
       <Header 
