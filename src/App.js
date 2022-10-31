d45a8e8638b8aec04a3502feec2af00540b10147
Opeyimika-sudo <54380080+Opeyimika-sudo@users.noreply.github.com>
2022-10-31 03:00:22 +0100

    feat: add state components in parent so children components can access

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import './App.css';
+import React from 'react'
 import Header from "./components/Header.js"
 import Todo from "./components/Todo.js"
 
 function App() {
+  const [toggleMode, setToggleMode] = React.useState(true)
+
+  function handleClick(){
+      setToggleMode(prevState => !prevState)
+  }
+  
+  const [formData, setFormData] = React.useState("")
+
+  function handleChange(event){
+      setFormData(event.target.value)
+      console.log(event.target.value)
+  }
+    
+  function handleSubmit(event){
+        event.preventDefault();
+        console.log(formData)
+    }
+
   return (
     <div className="App">
-      <h2>I am in love with my babe</h2>
-      <Header />
-      <Todo />
+      <Header 
+        toggleMode={toggleMode}
+        handleClick={handleClick}
+        formData={formData}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+      />
+      <Todo 
+        
+      />
     </div>
   );
 }
