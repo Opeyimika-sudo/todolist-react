7c4251c155e738ee15c3834c81acd20323ae7009
Opeyimika-sudo <54380080+Opeyimika-sudo@users.noreply.github.com>
2022-11-02 02:41:06 +0100

    feat: implement the form Input using localStorage with useState and useEffect

--- src/App.js
+++ src/App.js
@@ -4,34 +4,49 @@ import Header from "./components/Header.js"
 import Todo from "./components/Todo.js"
 
 function App() {
+  // const getItems = 
   const [toggleMode, setToggleMode] = React.useState(true)
 
   function handleClick(){
       setToggleMode(prevState => !prevState)
   }
   
-  const [todoList, setTodoList] = React.useState(localStorage.getItem('todos'))
-  const [formData, setFormData] = React.useState("")
+  const [todoList, setTodoList] = React.useState([])
+  
+  const [todoChecked, setTodoChecked] = React.useState(false)
 
+  const [todo, setTodo] = React.useState({
+      item: "",
+      todoChecked: todoChecked,
+  })
+
+  const [formData, setFormData] = React.useState("")
+  
   function handleChange(event){
     setFormData(event.target.value)
-    console.log(event.target.value)
+    console.log(formData)
   }
 
   function handleSubmit(event){
     event.preventDefault();
-    localStorage.setItem('todos', todoList == null ? formData : [todoList, formData]);
-}
+    setTodo((prevState) => {
+      return {
+        ...todo,
+        item: formData
+      }
+    })
+    console.log(todo)
+  }
 
   React.useEffect(() => {
-    setTodoList(localStorage.getItem('todos'));
-    console.log(todoList)
-  }, [localStorage.getItem('todos')])
+    console.log("I am in love")
+    localStorage.setItem('todo', todoList === null ? JSON.stringify([todo]) : JSON.stringify([...todoList, todo]));
+    setTodoList(JSON.parse(localStorage.getItem('todo')))
+  }, [todo])
 
-  
   return (
     <div className="App">
-      <Header 
+      <Header
         toggleMode={toggleMode}
         handleClick={handleClick}
         formData={formData}
