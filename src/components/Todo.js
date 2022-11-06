a301b804a37490247e4180e5c55590bf4b042393
Opeyimika-sudo <54380080+Opeyimika-sudo@users.noreply.github.com>
2022-11-06 23:27:39 +0100

    feat: add the toggle button to show completed and reflect the change in state

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,42 @@
 import React from 'react'
-import App from "../App"
+import App from "../App.js"
+
+
+
+// {todoList}
+// onClick={}
 
 export default function Todo(props) {
     console.log(props)
+    const todoList = 
+      props.todoList.map((item, id)=> (
+        <div>
+            <p className="todo_item" key = {id} id={id} style={props.toggleModeStyles} onClick={() => props.toggleCompleted(id, item)}>
+            {
+              item.checked
+              ? 
+              <span className="material-symbols-outlined" >
+                  check_circle
+              </span>
+              : 
+              <span className="material-symbols-outlined">
+              circle
+              </span> 
+            } 
+              { item.todo } 
+            {
+              <span class="material-symbols-outlined">
+                close
+              </span>
+            }
+            </p>
+        </div>
+      ))
+    
+    
   return (
     <div>
-        {props.todoList && props.todoList.map((item)=> <p className="todo_item">{item.item}</p>)}
+        {todoList}
         <p>It will take some time but my greatness will be undisputable</p>
     </div>
   )
