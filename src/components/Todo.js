bb5b1369744fcccbd6c162cce5aad54135ed5e17
Opeyimika-sudo <54380080+Opeyimika-sudo@users.noreply.github.com>
2022-11-09 04:29:25 +0100

    refactor: add divs, classNames and remove elements for better styling

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,107 +1,45 @@
 import React from 'react'
 import App from "../App.js"
+import '../App.css'
 
 export default function Todo(props) {
-      // const completed = document.
-      // if I click a button, I want to get a variation of todoList so what if I do an if-else statement where todoList means different things;
-    const presentActive = props.todoList.filter(todo => todo.checked === true).map((item, id)=> (
-          <p className="todo_item" key = {id} id={id} style={props.toggleModeStyles} >
+    const TODOLIST = props.todoList.map((item, id)=> (
+        <p className="todo_item" key =
+        {id}>
     {
       item.checked
       ? 
-      <span className="material-symbols-outlined" onClick={() => props.toggleCompleted(id, item)}>
+      <span className="material-symbols-outlined" id="check_circle" onClick={() => props.toggleCompleted(id, item)}>
           check_circle
       </span>
       : 
-      <span className="material-symbols-outlined" onClick={() => props.toggleCompleted(id, item)}>
+      <span className="material-symbols-outlined" id="check" onClick={() => props.toggleCompleted(id, item)}>
       circle
       </span> 
     } 
-      { item.todo } 
+      <span id="todo_text">{ item.todo } </span>
     {
-      <span className="material-symbols-outlined" onClick={()=> props.deleteTodo(id, item)}>
+      <span className="material-symbols-outlined"  id="delete" onClick={()=> props.deleteTodo(id, item)}>
         close
       </span>
     }
       </p>
-        ));
+      ));
 
-    const presentCompleted = props.todoList.filter(todo => todo.checked === true).map((item, id)=> (
-          <p className="todo_item" key = {id} id={id} style={props.toggleModeStyles} >
-    {
-      item.checked
-      ? 
-      <span className="material-symbols-outlined" onClick={() => props.toggleCompleted(id, item)}>
-          check_circle
-      </span>
-      : 
-      <span className="material-symbols-outlined" onClick={() => props.toggleCompleted(id, item)}>
-      circle
-      </span> 
-    } 
-      { item.todo } 
-    {
-      <span className="material-symbols-outlined" onClick={()=> props.deleteTodo(id, item)}>
-        close
-      </span>
-    }
-      </p>
-        ));
-
-    const presentAll = props.todoList.map((item, id)=> (
-          <p className="todo_item" key = {id} id={id} style={props.toggleModeStyles} >
-    {
-      item.checked
-      ? 
-      <span className="material-symbols-outlined" onClick={() => props.toggleCompleted(id, item)}>
-          check_circle
-      </span>
-      : 
-      <span className="material-symbols-outlined" onClick={() => props.toggleCompleted(id, item)}>
-      circle
-      </span> 
-    } 
-      { item.todo } 
-    {
-      <span className="material-symbols-outlined" onClick={()=> props.deleteTodo(id, item)}>
-        close
-      </span>
-    }
-      </p>
-        ));
-
-    let TODOLIST = presentAll;
-
-    function Active(){
-      TODOLIST = presentActive;
-      console.log("amazing")
-    }
-
-    function Completed(){
-      TODOLIST = presentCompleted;
-      console.log("sing");
-    }
-
-    function All(){
-      TODOLIST = presentAll;
-      console.log("yellow!")
-    }
 
     const todoListFilters = 
       <div className="filters">
-              {props.todoList.length > 0 && <p> {props.todoList.length > 1 ? `${props.todoList.length} Items Left`: `${props.todoList.length} Item Left`}</p>}
-              <p onClick={() => All()}>All</p>
-              <p onClick={() => Active()}>Active</p>
-              <p onClick={() => Completed()}>Completed</p>
-              <p onClick={() => props.clearCompleted()}>Clear Completed</p>
+              {props.todoList.length > 0 && <p> {props.todoList.filter(item => item.checked === false).length > 1 ? `${props.todoList.filter(item => item.checked === false).length} Items Left`: `${props.todoList.filter(item => item.checked === false).length} Item Left`}</p>}
+              <p className="filter-btn">All</p>
+              <p className="filter-btn">Active</p>
+              <p className="filter-btn">Completed</p>
+              <p className="filter-btn" onClick={()=> props.clearCompleted()}>Clear Completed</p>
       </div>;
     
     return (
-      <div>
+      <div className="todoList" style={props.toggleModeStyles}>
           {TODOLIST}
-          {console.log(TODOLIST)}
           {todoListFilters}
-          <p>It will take some time but my greatness will be undisputable</p>
       </div>
     )
 }
