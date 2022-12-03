a85fb75e9441ddc37982defece7560285352536c
Opeyimika-sudo <54380080+Opeyimika-sudo@users.noreply.github.com>
2022-12-03 03:26:56 +0100

    feat: create a Filter for the active todos

---
+++ src/components/Filters/Active.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function Active(props) {
+  return (
+    <div style={props.toggleModeStyles}>
+      {props.todoList.filter(item => item.checked === false).map((item, id)=> (
+        <p className="todo_item" key =
+        {id}>
+    {
+      item.checked
+      ? 
+      <span className="material-symbols-outlined" id="check_circle" onClick={() => props.toggleCompleted(id, item)}>
+          check_circle
+      </span>
+      : 
+      <span className="material-symbols-outlined" id="check" onClick={() => props.toggleCompleted(id, item)}>
+      circle
+      </span> 
+    } 
+      <span id="todo_text">{ item.todo } </span>
+    {
+      <span className="material-symbols-outlined"  id="delete" onClick={()=> props.deleteTodo(id, item)}>
+        close
+      </span>
+    }
+      </p>
+      ))}
+    </div>
+  )
+}
