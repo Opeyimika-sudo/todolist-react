4e8eb4b00d44d017efa2fff825a487624723d16d
Opeyimika-sudo <54380080+Opeyimika-sudo@users.noreply.github.com>
2022-12-03 03:29:28 +0100

    feat: implement the logic to switch between filters and for the font-color of the filter to change

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,43 +1,54 @@
 import React from 'react'
 import '../App.css'
+import All from './Filters/All'
+import Active from './Filters/Active'
+import Completed from './Filters/Completed'
+
 
 export default function Todo(props) {
-    const TODOLIST = props.todoList.map((item, id)=> (
-        <p className="todo_item" key =
-        {id}>
-    {
-      item.checked
-      ? 
-      <span className="material-symbols-outlined" id="check_circle" onClick={() => props.toggleCompleted(id, item)}>
-          check_circle
-      </span>
-      : 
-      <span className="material-symbols-outlined" id="check" onClick={() => props.toggleCompleted(id, item)}>
-      circle
-      </span> 
-    } 
-      <span id="todo_text">{ item.todo } </span>
-    {
-      <span className="material-symbols-outlined"  id="delete" onClick={()=> props.deleteTodo(id, item)}>
-        close
-      </span>
+    const toggleModeFilter = {
+      backgroundColor: props.toggleMode ? "#f0f0f0" : "#25283c", 
+      color: props.toggleMode ? "black" : "#fff"
     }
-      </p>
-      ));
-
-
     const todoListFilters = 
       <div className="filters">
               {props.todoList.length > 0 && <p> {props.todoList.filter(item => item.checked === false).length > 1 ? `${props.todoList.filter(item => item.checked === false).length} Items Left`: `${props.todoList.filter(item => item.checked === false).length} Item Left`}</p>}
-              <p className="filter-btn">All</p>
-              <p className="filter-btn">Active</p>
-              <p className="filter-btn">Completed</p>
-              <p className="filter-btn" onClick={()=> props.clearCompleted()}>Clear Completed</p>
+              <p 
+              style={{color: props.filters === "All" ?  "blue" : toggleModeFilter.color}}
+              onClick={() => props.handleFilterClick(1)}>All</p>
+              <p 
+              style={{color: props.filters === "Active"? "blue" : toggleModeFilter.color}} 
+              onClick={() => props.handleFilterClick(2)}>Active</p>  
+              <p 
+              style={{color: props.filters === "Completed" ? "blue" : toggleModeFilter.color}}
+              onClick={() => props.handleFilterClick(3)}>Completed</p>
+              <p onClick={()=> props.clearCompleted()}>Clear Completed</p>
       </div>;
     
     return (
-      <div className="todoList" style={props.toggleModeStyles}>
-          {TODOLIST}
+      <div className="todoList" style={toggleModeFilter}>
+        {/* Filters */}
+          {(props.filters === "" || props.filters === "All") && <All
+            todoList={props.todoList}
+            toggleModeStyles={props.toggleModeStyles}
+            toggleCompleted={props.toggleCompleted}
+            deleteTodo={props.deleteTodo}
+            clearCompleted={props.clearCompleted}
+          />}
+          {props.filters === "Active" && <Active 
+            todoList={props.todoList}
+            toggleModeStyles={props.toggleModeStyles}
+            toggleCompleted={props.toggleCompleted}
+            deleteTodo={props.deleteTodo}
+            clearCompleted={props.clearCompleted}
+            />}
+          {props.filters === "Completed" && <Completed 
+            todoList={props.todoList}
+            toggleModeStyles={props.toggleModeStyles}
+            toggleCompleted={props.toggleCompleted}
+            deleteTodo={props.deleteTodo}
+            clearCompleted={props.clearCompleted}
+          />} 
           {todoListFilters}
       </div>
     )
